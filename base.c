6152c0ab15f8888d9469513899cdf1da6c11d76c
Penrose480 <Penrose480@proton.me>
2026-07-14 19:42:53 -0700

    updated base.c

--- base.c
+++ base.c
@@ -10,31 +10,31 @@ int main(void)
     char input[100];
 
     while (1) {
-	printf("? ");
-	fflush(stdout);
-	if (fgets(input, sizeof(input), stdin) == NULL)
-	    break;
+        printf("? ");
+        fflush(stdout);
+        if (fgets(input, sizeof(input), stdin) == NULL)
+            break;
 
-	input[strcspn(input, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0';
 
-	if (strcmp(input, "exit") == 0) {
-	    exit(0);
-	}
-	
-	pid_t child = fork();
-	
-	if (child == -1) {
-	    perror("fork");
-	    exit(EXIT_FAILURE);
-	}
+        if (strcmp(input, "exit") == 0) {
+            exit(0);
+        }
+        
+        pid_t child = fork();
+        
+        if (child == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
 
-	if (child == 0) {
-	    execlp(input, input, NULL);
-	    perror(input);
-	    exit(EXIT_FAILURE);
-	}
+        if (child == 0) {
+            execlp(input, input, NULL);
+            perror(input);
+            exit(EXIT_FAILURE);
+        }
 
-	wait(NULL);
+        wait(NULL);
     }
     return 0;
 }
